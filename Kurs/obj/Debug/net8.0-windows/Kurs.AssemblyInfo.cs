6bf1e67e61129a9a0ea19541bc50ab1faa1a9fd1
MollanX <maximpure2016@gmail.com>
2024-06-06 09:26:11 +0500

    ти

--- Kurs/obj/Debug/net8.0-windows/Kurs.AssemblyInfo.cs
+++ Kurs/obj/Debug/net8.0-windows/Kurs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kurs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+038a33b8f7c75808194639602d8ded4c6a670991")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kurs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kurs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
